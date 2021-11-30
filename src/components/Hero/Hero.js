8065e5d9d5f40b39d8bc920ab1012ec5090b9874
SuryaRamanath <vrsurya80@gmail.com>
2021-11-30 15:28:23 +0530

    slider images change

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./Hero.scss";
-import slider1 from '../../assets/slider.jpg'
-import slider2 from '../../assets/slider2.jpg'
-import slider5 from '../../assets/slider5.jpg'
+import slider1 from '../../assets/np1.jpeg'
+import slider2 from '../../assets/np2.jpg'
+import slider3 from '../../assets/np3.jpeg'
 import slider4 from '../../assets/slider4.jpeg'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
@@ -17,13 +17,13 @@ export default function Hero() {
         autoPlay={true}
         interval={2000}>
         <div>
+          <img src={slider1} />
+        </div>
+        <div>
           <img src={slider2} />
         </div>
         <div>
-          <img src={slider5} />
-        </div>
-        <div>
-          <img src={slider4} />
+          <img src={slider3} />
         </div>
       </Carousel>
     </div>
