d8c930d68e9b0b20391d0aa93df9047310474d51
SuryaRamanath <vrsurya80@gmail.com>
2021-11-30 21:53:04 +0530

    about section

--- src/components/AboutUs/About.js
+++ src/components/AboutUs/About.js
@@ -4,8 +4,12 @@ import "./About.scss"
 
 export default function About() {
     return (
-        <div>
-            this is about
+        <div className="about-container">
+           <h1>About Us</h1>
+           <p>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vivamus at augue eget arcu. Risus nullam eget felis eget nunc lobortis mattis. Tempor orci eu lobortis elementum. A diam sollicitudin tempor id eu nisl nunc mi. Rhoncus mattis rhoncus urna neque viverra justo. Lectus vestibulum mattis ullamcorper velit sed ullamcorper. Sed viverra tellus in hac. Molestie nunc non blandit massa enim. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Eu nisl nunc mi ipsum faucibus vitae aliquet. Sed odio morbi quis commodo odio aenean sed. Pellentesque habitant morbi tristique senectus et netus. Quisque egestas diam in arcu cursus. Ultrices tincidunt arcu non sodales neque.
+           </p>
         </div>
+        
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
     <section>
       <Navbar />
       <Hero />
+      <About />
     </section>
   )
 }
